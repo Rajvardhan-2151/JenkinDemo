da789b66252dadb563f64e2e9933fbd39b1ade56
Rajvardhan Patil <rajbpatil8623@gmail.com>
2022-01-24 15:05:40 +0530

    Added java demo application files

---
+++ src/test/java/study/JavaDockerApplicationTests.java
@@ -0,0 +1,20 @@
+package study;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+	
+	@Test
+	public void testInputIsEven(){
+	 assertTrue(JavaDockerApplication.checkIfInputIsAnEvenNumber(22)); //Assertion
+	 }
+
+}
